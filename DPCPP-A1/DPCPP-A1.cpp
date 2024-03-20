25b1a21b4782fb26d00112b5ffa38aa4f832b567
Matthew Freak <fremk005@mymail.unisa.edu.au>
2024-03-20 21:53:41 +1030

    Add project files.

---
+++ DPCPP-A1/DPCPP-A1.cpp
@@ -0,0 +1,12 @@
+﻿// DPCPP-A1.cpp : Defines the entry point for the application.
+//
+
+#include "DPCPP-A1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
